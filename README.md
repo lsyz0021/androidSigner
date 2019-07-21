581f651a373f0b9ad3c13698d4d89fa07354f3f9
lichuanwu <lichuanwu0021@163.com>
2019-07-21 16:09:58 +0800

    新增repeatSigner、searchKey配置

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# androidSigner
 **转载请标注原创地址：[https://blog.csdn.net/lsyz0021/article/details/96499543](https://blog.csdn.net/lsyz0021/article/details/96499543)**
 
-一提到给apk签名，大家或许想这还不简单，打开终端配置好“apksigner”命令一行不就搞定了，但是如果让你给100个apk签名，这样的方式好简单吗？
+[GitHub地址 https://github.com/lsyz0021/androidSigner](https://github.com/lsyz0021/androidSigner)
+
+
+一提到给apk签名，大家或许想这还不简单，打开终端配置好“apksigner”命令一行不就搞定了，但是如果让你给100个apk签名，这样的签名方式还简单吗？
 因为最近有经常要给apk签名的需要，并且有时候可能要同时给十几个apk签名，所以就想到了写个批量v2签名的shell脚本，写完之后使用感觉起来感觉也特别方便。想到或许其他人也可能会有这种需求，于是将他开源出来供大家使用。废话不多说先介绍功能！
 
 ## 1、设置配置文件
@@ -21,6 +23,10 @@ keyPassword=12345678
 buildTools=/Users/wuge/sdk/build-tools/29.0.0/
 #是否自动搜索指定目录，true:自动搜索，false:手动输入，no:需要选择获取方式
 auto=no
+#是否重复签名，已签名的apk名字上会加个“signer-”，true：如果搜索到会对该apk再次签名，false：不会再签名
+repeatSigner=false
+#指定搜索的apk的关键字(支持通配符*)
+searchKey=app-release*.apk
 #指定搜索目录，"."代表当前目录，不设置此值默认为当前目录
 searchPath=.
 #指定输出目录，不设置默认为当前目录下的out目录
@@ -28,16 +34,22 @@ outPath=./out
 ```
 ## 2、执行脚本，选择“y”
 配置好上面的内容之后，就可以执行`signer.sh`脚本了。配置文件默认`auto=no`，所以需要手动选择输入方式，下面是输入的`y`，开始在指定目录(`searchPath=.`表示当前目录)下开始自动搜索所有的apk，并且进行签名。
-**注意**：如果`auto=true`，相当于输入了`y`，自动就进行搜索指定目录(`searchPath=.`)下的apk
+**注意：如果`auto=true`，相当于自动输入了`y`，就自动进行搜索指定目录(`searchPath=.`)下的apk**
 ![在这里插入图片描述](https://img-blog.csdnimg.cn/20190719205719878.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xzeXowMDIx,size_16,color_FFFFFF,t_70)
+
 ## 3、生成获取签名apk
-执行完`signer.sh`脚本，在指定的输出目录(`outPath=./out`)下生成了签名后的apk，每个签名成功的apk名字前面都会增加"signer-"前缀。
+执行完`signer.sh`脚本，在指定的输出目录(`outPath=./out`)下生成了签名后的apk，每个签名成功的apk名字前面都会增加"signer-"前缀。**默认情况下如果搜索到以signer-开头的apk不会重复签名，如果想重复签名则设置`repeatSigner=true`**
 ![在这里插入图片描述](https://img-blog.csdnimg.cn/20190719205757300.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xzeXowMDIx,size_16,color_FFFFFF,t_70)
+
 ## 4、执行脚本，选择“n”
-这里我们输入`n`，就需要我们手动指定apk的路径了
-**注意**：如果`auto=false`，相当于输入了`n`
+运行`signer.sh`，我们输入`n`，这时就需要我们手动指定apk的路径了
+**注意**：如果`auto=false`，相当于自动输入了`n`
 ![在这里插入图片描述](https://img-blog.csdnimg.cn/20190719205807907.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xzeXowMDIx,size_16,color_FFFFFF,t_70)
+根据自己的需求配置`signerConfig.ini`，即可灵活使用了。
+
 [GitHub地址 https://github.com/lsyz0021/androidSigner](https://github.com/lsyz0021/androidSigner)
+
+
 
 
 
--- signer.sh
+++ signer.sh
@@ -6,7 +6,10 @@ storeFile="" # 签名文件的路径
 storePassword="" # 签名文件的密码
 keyAlias="" # key的别名
 keyPassword="" # key的密码
+
 auto="false" # 是否自动搜索
+repeatSigner="false" # 是否重复签名，，已完成签名的apk名字上会加个“signer”，true：会再签名，false：不会再签名
+searchKey="*.apk"
 configSearchPath="" # 指定搜索的目录，不指定默认问当前目录
 configOutPath="" # 指定签名完的apk输出目录，不指定默认为当前目录下的out目录
 
@@ -16,29 +19,78 @@ errorTimes=0 # 失败的签名次数
 findApkLastResult="" # 获取查找结果的最后一个apk
 findApkArray=() #查找到的所有apk的数组
 
-# 获取配置文件的内容
-function findConfigFile()
+isShowLog="true" # 是否显示log
+
+log()
 {
-    if [ -f "$configPath" ]; then
-        storeFile=`cat $configPath | grep storeFile | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        storePassword=`cat $configPath | grep storePassword | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        keyAlias=`cat $configPath | grep keyAlias | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        keyPassword=`cat $configPath | grep keyPassword | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        androidCommandPath=`cat $configPath | grep buildTools | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        auto=`cat $configPath | grep auto | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        configSearchPath=`cat $configPath | grep searchPath | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
-        configOutPath=`cat $configPath | grep outPath | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    msg=$1
+
+    if [[ "$isShowLog" != "false" ]]; then
+        echo -e "$msg"
+    fi
+}
+
+# 获取配置文件的内容
+function getConfigFileInfo()
+{
+    storeFile=`cat $configPath | grep storeFile | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    storePassword=`cat $configPath | grep storePassword | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    keyAlias=`cat $configPath | grep keyAlias | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    keyPassword=`cat $configPath | grep keyPassword | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    androidCommandPath=`cat $configPath | grep buildTools | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    auto=`cat $configPath | grep auto | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    repeatSigner=`cat $configPath | grep repeatSigner | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    searchKey=`cat $configPath | grep searchKey | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    configSearchPath=`cat $configPath | grep searchPath | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+    configOutPath=`cat $configPath | grep outPath | awk -F'=' '{ print $2 }' | sed s/[[:space:]]//g`
+
+    # 检查命令行目录是否存在
+    if [[ ! -d "${androidCommandPath}" ]]; then
+        echo -e "\n错误！配置的build-tools路径找不到，请检查${configPath}文件中buildTools的设置\n"
+        exit 0
+    fi
+    # 检查zipalign命令是否存在
+    zipalign="${androidCommandPath}/zipalign"
+    if [[ ! -f "$zipalign" ]]; then
+        echo -e "\n错误！${zipalign} 命令找不到\n"
+        exit 0
+    fi
+    # 检查apksigner命令是否存在
+    apksigner="${androidCommandPath}/apksigner"
+    if [[ ! -f "$apksigner" ]]; then
+        echo -e "\n错误！${apksigner} 命令找不到，apksigner命令在build-tools 25.0.0才出现\n"
+        exit 0
+    fi
+
+    if [[ "$repeatSigner" != "true" ]]; then
+        repeatSigner="false"
+    fi
+    if [[ -z "$searchKey" ]]; then
+        searchKey="*.apk"
+    fi
+}
+
+# 获取手动输入的信息
+function getInputConfigInfo()
+{
+    read -e -p "第1步(共5部)请输入签名文件地址:" storeFile
+    read -p "第2步(共5部)请输入签名密码:" -s storePassword    
+    echo ""
+    read -p "第3部(共5部)请输入key的别名:" keyAlias
+    read -p "第4部(共5部)请输入key的密码:" -s keyPassword
+    echo ""
+    read -e -p "第5步(共5部)请输入build-tools路径:" androidCommandPath
+}
+
+# 选择获取配置信息的方式
+function processConfigInfo()
+{
+    if [[ -f "$configPath" ]]; then
+        getConfigFileInfo
     else
         echo -e "\n当前目录下找不到配置文件${configPath},需要手动输入账号和密码，\n强烈建议在当前目录下创建${configPath}配置文件\n"
-
-        read -e -p "第1步(共5部)请输入签名文件地址:" storeFile
-        read -p "第2步(共5部)请输入签名密码:" -s storePassword    
-        echo ""
-        read -p "第3部(共5部)请输入key的别名:" keyAlias
-        read -p "第4部(共5部)请输入key的密码:" -s keyPassword
-        echo ""
-        read -e -p "第5步(共5部)请输入build-tools路径:" androidCommandPath
-
+        getInputConfigInfo 
+        repeatSigner="true" # 如果手动指定apk路径，则重复签名
     fi
 }
 
@@ -67,81 +119,64 @@ function signAndroidApk()
     else
         apkPath=${sourceApk%/*}
         apkName=${sourceApk##*/}
+        # 创建输出的目录
+        if [[ -z "$outputPath" ]]; then  # outputPath的值为空
+            outputPath=${apkPath}/out
+            if [ ! -d "$outputPath" ]; then
+                mkdir -p $outputPath
+                echo 没有指定输出路径,则输出到apk所在目录的out目录:$outputPath
+            fi 
+        elif [[ ! -d "$outputPath" ]]; then # outputPath目录不存在
+            echo 由于${outputPath}路径不存在,则创建该路径。
+            mkdir -p $outputPath
 
-        if [[ "$sourceApk" =~ "signer" ]];then
-            echo "注意啦！第${tatalTimes}个Apk已签名,不需要再签名了:$sourceApk"
-            let errorTimes++
-        else
-            # 创建输出的目录
-            if [[ -z "$outputPath" ]]; then  # outputPath的值为空
+            # 创建目录失败
+            if [[ ! -d "$outputPath" ]]; then
+                echo 创建${outputPath}目录失败，则输出到apk所在目录的out目录
                 outputPath=${apkPath}/out
                 if [ ! -d "$outputPath" ]; then
                     mkdir -p $outputPath
-                    echo 没有指定输出路径,则输出到apk所在目录的out目录:$outputPath
-                fi 
-            elif [[ ! -d "$outputPath" ]]; then # outputPath目录不存在
-                echo 由于${outputPath}路径不存在,则创建该路径。
-                mkdir -p $outputPath
-
-                # 创建目录失败
-                if [[ ! -d "$outputPath" ]]; then
-                    echo 创建${outputPath}目录失败，则输出到apk所在目录的out目录
-                    outputPath=${apkPath}/out
-                    if [ ! -d "$outputPath" ]; then
-                        mkdir -p $outputPath
-                    fi
                 fi
             fi
+        fi
 
-            # 检查命令行目录是否存在
-            if [[ ! -d "${androidCommandPath}" ]]; then
-                echo -e "\n错误！配置的build-tools路径找不到，请检查${configPath}文件中buildTools的设置\n"
-                exit 0
-            fi
-            # 检查zipalign命令是否存在
-            zipalign=$androidCommandPath/zipalign
-            if [[ ! -f "$zipalign" ]]; then
-                echo -e "\n错误！${zipalign} 命令找不到\n"
-                exit 0
-            fi
-            # 检查apksigner命令是否存在
-            apksigner=$androidCommandPath/apksigner
-            if [[ ! -f "$apksigner" ]]; then
-                echo -e "\n错误！${apksigner} 命令找不到，apksigner命令在build-tools 25.0.0才出现\n"
-                exit 0
+        # 检查命令行目录是否存在
+        if [[ ! -d "${androidCommandPath}" ]]; then
+            echo -e "\n错误！配置的build-tools路径找不到，请检查${configPath}文件中buildTools的设置\n"
+            exit 0
+        fi
+
+        zipalignApk="$outputPath/zipalign-$apkName"
+        signerApk="$outputPath/signer-$apkName" 
+
+        # 开始4K对齐
+        #zipalign=$androidCommandPath/zipalign
+        $androidCommandPath/zipalign -f 4 $sourceApk $zipalignApk
+        
+        if [[ ! -f "$zipalignApk"  || ! -s "${zipalignApk}" ]];then # 对齐完的apk不是一个文件，或者大小为0kb
+            echo "注意啦！第${tatalTimes}个apk 4K对齐失败${zipalignApk}"
+            let errorTimes++
+            rm -f $zipalignApk
+        else
+            # 4K对齐成功，开始V2签名
+            #apksigner=$androidCommandPath/apksigner
+            $androidCommandPath/apksigner sign --ks $storeFile --ks-pass pass:$storePassword --ks-key-alias $keyAlias --key-pass pass:$keyPassword --out $signerApk $zipalignApk
+            # 签名完，删除对齐过程中生成的apk
+            rm -f $zipalignApk
+            if [  -f "$zipalignApk" ];then
+                echo -e "\n注意！对齐生成的apk删除失败，请手动删除:${zipalignApk}\n"
             fi
 
-            zipalignApk="$outputPath/zipalign-$apkName"
-            signerApk="$outputPath/signer-$apkName" 
-
-            # 开始4K对齐
-            $zipalign -f 4 $sourceApk $zipalignApk
-            
-            if [[ ! -f "$zipalignApk"  || ! -s "${zipalignApk}" ]];then # 对齐完的apk不是一个文件，或者大小为0kb
-                echo "注意啦！第${tatalTimes}个apk 4K对齐失败${zipalignApk}"
+            if [ ! -f "$signerApk" ]; then
+                echo -e "\n注意啦！第${tatalTimes}个apk签名失败，没有生成签名的apk\n\n"
+               let errorTimes++
+            elif [ ! -s "$signerApk" ];then # 生成的apk大小为0kb
+                echo -e "\n注意啦！第${tatalTimes}个apk签名失败，因为生成签名的apk大小为0KB\n\n"
                 let errorTimes++
-                rm -f $zipalignApk
+                rm -f $signerApk
             else
-                # 4K对齐成功，开始V2签名
-                $apksigner sign --ks $storeFile --ks-pass pass:$storePassword --ks-key-alias $keyAlias --key-pass pass:$keyPassword --out $signerApk $zipalignApk
-                # 签名完，删除对齐过程中生成的apk
-                rm -f $zipalignApk
-                if [  -f "$zipalignApk" ];then
-                    echo -e "\n注意！对齐生成的apk删除失败，请手动删除:${zipalignApk}\n"
-                fi
-
-                if [ ! -f "$signerApk" ]; then
-                    echo -e "\n注意啦！第${tatalTimes}个apk签名失败，没有生成签名的apk\n\n"
-                   let errorTimes++
-                elif [ ! -s "$signerApk" ];then # 生成的apk大小为0kb
-                    echo -e "\n注意啦！第${tatalTimes}个apk签名失败，因为生成签名的apk大小为0KB\n\n"
-                    let errorTimes++
-                    rm -f $signerApk
-                else
-                    echo V2签名成功:$signerApk                    
-                fi
+                echo V2签名成功:$signerApk                    
             fi
-
         fi
     fi
 }
@@ -160,7 +195,7 @@ function getApk()
     if [[ "$auto" == "true" ]]; then
         # 1、自动搜索apk
         echo -e "\n${configPath}中设置auto=true，开始自动搜索${findPath}目录"
-        findApk $findPath "*.apk"
+        findApk $findPath $searchKey
     elif [[ "$auto" == "false" ]]; then
          # 2、手动指定apk路径
         echo -e "\n${configPath}中设置auto=false，需要手动输入apk路径"
@@ -170,19 +205,19 @@ function getApk()
         findApkArray[0]=$inputApkPath #手动指定apk
     else
         # auto!=false或者auto!=true，就会执行这里
-        echo -e "\n---------------警告----------------"
+        echo -e "\n---------------提示----------------"
         echo -e "${configPath}配置中auto=${auto}，需要选择要执行的的方式，
                 \n如果配置auto=true则自动搜索，auto=false则手动输入。"
-        echo -e "---------------警告----------------\n"
+        echo -e "---------------提示----------------\n"
         
         # 选择方式
         echo ""
-        read -n1 -p "是否自动搜索指定目录(如果没指定搜索目录，默认搜索当前目录)下的apk，否则要手动指定apk路径, [Y/N]?" answer
+        read -n1 -p "是否自动搜索指定目录(默认搜索当前目录)下的apk，否则要手动指定apk路径, [Y/N]?" answer
         case $answer in
         Y | y)
             # 3、自动搜索apk
             echo -e "\n开始自动搜索${findPath}目录下的apk"
-            findApk $findPath "*.apk" 
+            findApk $findPath $searchKey
             ;;
         N | n)
             # 4、手动指定apk路径
@@ -205,7 +240,16 @@ function processApk()
     do
         let tatalTimes++
         echo -e "\n\n准备开始签名第${tatalTimes}个Apk =$apk"
-        signAndroidApk $apk $configOutPath
+        
+        # 已签名的apk是否再次签名
+        if [[ "$apkName" == signer-*.apk && "$repeatSigner" == "false" ]]; then
+            echo -e "注意啦！第${tatalTimes}个Apk已签名,不需要再签名了:$sourceApk
+                \n如果想重复签名请将repeatSigner=true"
+            let errorTimes++
+        else
+            signAndroidApk $apk $configOutPath
+        fi
+        
     done
 
     #不需要循环数组，直接使用数组中最后一个结果进行签名
@@ -219,7 +263,7 @@ function processApk()
 # 程序运行入口
 
 # 1、获取配置文件的内容
- findConfigFile
+ processConfigInfo
 
 # 2、搜索apk（根据配置内容选择要获取apk的方式）
  getApk
@@ -230,5 +274,5 @@ function processApk()
 
 
 
- 
+
 
